4f15d75b76202f88f6574fdd19b55a1e23367473
css_hc <huangchao@css.com.cn>
2019-07-24 17:03:00 +0800

    测试代码提交

--- wgjc_web/src/test/java/com/wgjc/student/test/StudentTest.java
+++ wgjc_web/src/test/java/com/wgjc/student/test/StudentTest.java
@@ -4,14 +4,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.github.pagehelper.PageHelper;
 import com.wgjc.SpringbootWgjcApplication;
 import com.wgjc.base.entity.PageRequest;
 import com.wgjc.base.entity.PageResult;
 import com.wgjc.base.util.PageUtils;
+import com.wgjc.test.controller.StudentController;
 import com.wgjc.test.dao.StudentMapper;
 import com.wgjc.test.entity.Student;
 import com.wgjc.test.service.StudentService;
@@ -22,6 +31,12 @@ public class StudentTest {
 	
 	@Autowired
 	private StudentService studentService;
+	
+	@Value("${server.port}")
+	private int port;
+	
+	@Autowired
+	private StudentController  controller;
 	
 	@Test
 	public void test() {
@@ -45,5 +60,22 @@ public class StudentTest {
 		System.out.println(result.toString());
 	}
 	
-	
+	@Test
+	public void testAnnotation() {
+		System.out.println(port);
+		
+		MockMvc mvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+	     RequestBuilder builder = MockMvcRequestBuilders
+	                .get("/getallStudent")
+	                .accept(MediaType.APPLICATION_JSON)
+	                .contentType(MediaType.APPLICATION_JSON_UTF8);
+	        MvcResult result;
+			try {
+				result = mvc.perform(builder).andReturn();
+				System.out.println(result.getResponse().getContentAsString());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
 }
