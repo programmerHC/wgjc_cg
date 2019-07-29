5b6b8a0f3bf3261f8cf047878a77177a3091de82
liqiangmei <2657073963@QQ.COM>
2019-07-29 08:06:09 +0800

    登录系统设计

---
+++ wgjc_entity/src/main/java/com/wgjc/customer/entity/Customer.java
@@ -0,0 +1,52 @@
+package com.wgjc.customer.entity;
+
+/** 
+ * @Description: 用户类封装
+ * @author hc
+ * @date 2019年7月22日上午11:19:33
+ */
+public class Customer {
+	private String uuid;
+	private String username;
+	private String realname;
+	private String password;
+	private String phone;
+	private String adress;
+	
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ wgjc_interceptor/src/main/java/com/wgjc/login/annotation/Login.java
@@ -0,0 +1,21 @@
+package com.wgjc.login.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * @Description:类和方法添加登录权限验证
+ * @author hc
+ * @da2019年7月28日
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface Login {
+
+}
